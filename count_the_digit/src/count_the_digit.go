6e61ed0c534a2e396f4fa06e336ad6a9e6e78344
kujo <80rN70c0D3@protonmail.com>
2020-09-27 04:28:15 +0200

    update

--- count_the_digit/src/count_the_digit.go
+++ count_the_digit/src/count_the_digit.go
@@ -5,7 +5,7 @@ import (
 	"strings"
 )
 
-//NbDig square all number between 0 and n. Count number of d
+//NbDig square all numbers between 0 and n and count the number of d
 func NbDig(n int, d int) (count int) {
 	for i := 0; i <= n; i++ {
 		count += strings.Count(strconv.Itoa(i*i), strconv.Itoa(d))
